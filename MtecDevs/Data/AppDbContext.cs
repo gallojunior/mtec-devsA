0dcb5a66b495252498c796bbf80ce8602cb9f078
GalloJunior <gallojunior@gmail.com>
2023-09-25 09:38:01 -0300

    Criado a migração

--- MtecDevs/Data/AppDbContext.cs
+++ MtecDevs/Data/AppDbContext.cs
@@ -13,4 +13,9 @@ public class AppDbContext : IdentityDbContext
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<TipoDev> TipoDevs { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
 }
